d8984d5b5ed0152994fb83c6c1e4830db6f3c2df
rohaan-gh <96631138+rohaan-gh@users.noreply.github.com>
2022-01-05 09:53:16 +0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,8 +2,8 @@
 clear
 echo " 
       Installing...."
-sleep 4
-apt update && apt upgrade && apt install whois && apt install curl && apt install jq && apt install nikto
+sleep 2
+apt update && apt upgrade && apt install whois && apt install curl && apt install jq && apt install nikto && apt install wpscan && apt install uniscan
 echo "
       Done"
 
